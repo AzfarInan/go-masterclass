36907bc4c401de2f3474a921dce158ff46734a2b
Azfar Inan <azfar.inan0615@gmail.com>
2024-02-14 23:01:10 +0600

    Added More Unit Tests

--- simplebank/db/sqlc/account_test.go
+++ simplebank/db/sqlc/account_test.go
@@ -49,3 +49,79 @@ func TestGetAccount(t *testing.T) {
 	require.NotZero(t, account2.ID)
 	require.NotZero(t, account2.CreatedAt)
 }
+
+func TestUpdateAccount(t *testing.T) {
+	createArg := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+
+	account1, err := testQueries.CreateAccount(context.Background(), createArg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account1)
+
+	updateArg := UpdateAccountParams{
+		ID:      account1.ID,
+		Balance: util.RandomMoney(),
+	}
+
+	account2, err := testQueries.UpdateAccount(context.Background(), updateArg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Currency, account2.Currency)
+
+	require.NotEqual(t, account1.Balance, account2.Balance)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	createArg := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+
+	account1, err := testQueries.CreateAccount(context.Background(), createArg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account1)
+
+	err = testQueries.DeleteAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.Error(t, err)
+	require.Empty(t, account2)
+}
+
+func TestListAccounts(t *testing.T) {
+	for i := 0; i < 5; i++ {
+		arg := CreateAccountParams{
+			Owner:    util.RandomOwner(),
+			Balance:  util.RandomMoney(),
+			Currency: util.RandomCurrency(),
+		}
+
+		account, err := testQueries.CreateAccount(context.Background(), arg)
+		require.NoError(t, err)
+		require.NotEmpty(t, account)
+
+		require.NotZero(t, account.ID)
+		require.NotZero(t, account.CreatedAt)
+	}
+
+	arg := ListAccountsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
+}
